4a8aa988d39b695f5cc3936c53f6e7f224fcd8f7
Arthur Guz <zugruhtra@gmail.com>
2023-01-24 20:18:57 +0100

    changed cli.py

--- tcalc/cli.py
+++ tcalc/cli.py
@@ -1,27 +1,21 @@
 import sys
 import argparse
 
-from tcalc.token import tokenize
-from tcalc.calc import (calc_infix, calc_postfix)
-from tcalc.exception import ParseError
+from tcalc.reader import InputReader
+from tcalc.lexer import Lexer
+from tcalc.parser import Parser
+from tcalc.evaluator import Evaluator
+from tcalc.errors import LexerError, ParserError
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.prog = 'tcalc'
+    parser.prog = "tcalc"
     parser.formatter_class = argparse.RawDescriptionHelpFormatter
-    parser.description = 'Calculate with Time Expressions'
+    parser.description = "Calculate with Time Expressions"
 
     parser.add_argument(
-        'expr',
-        type=str,
-        help='a time expression. Use - to read from stdin'
-    )
-
-    parser.add_argument(
-        '--postfix', '-p',
-        action='store_true',
-        help='evaluate postfix expression'
+        "expr", type=str, nargs="?", help="a time expression. Use - to read from stdin"
     )
 
     return parser.parse_args()
@@ -30,29 +24,46 @@ def get_args():
 # Main
 
 
-def main():
-    args = get_args()
-    expr = args.expr if args.expr != '-' else sys.stdin.read()
-
-    if args.postfix:
-        calc = calc_postfix
-    else:
-        calc = calc_infix
-
+def evaluate(expr: str) -> int:
     try:
-        result = calc(tokenize(expr))
-    except ParseError as err:
-        sys.stderr.write('{}\n'.format(err))
+        input_reader = InputReader(expr)
+        lexer = Lexer(input_reader)
+        parser = Parser(lexer)
+        evaluator = Evaluator(parser)
+        result = evaluator.eval()
+    except ParserError as err:
+        sys.stderr.write(expr + "\n")
+        sys.stderr.write(" " * (err.column - 1) + "^\n")
+        sys.stderr.write(err.msg + "\n")
         return 1
+    except LexerError as err:
+        sys.stderr.write(err.msg + "\n")
+        return 3
     except ZeroDivisionError:
-        sys.stderr.write('Divison by Zero!\n')
+        sys.stderr.write("Divison by Zero!\n")
         return 2
 
-    sys.stdout.write('{}'.format(result))
+    sys.stdout.write("{}\n".format(result))
 
     return 0
 
 
-if __name__ == '__main__':
-    sys.exit(main())
+def main():
+    args = get_args()
+    expr = args.expr if args.expr != "-" else sys.stdin.read()
+    if not expr:
+        while True:
+            sys.stdout.write("tcalc> ")
+            sys.stdout.flush()
+            expr = sys.stdin.readline().strip()
+            if expr.lower() in ("exit", "quit", "halt"):
+                return 0
+            rc = evaluate(expr)
+            if rc != 0:
+                return rc
+    else:
+        return evaluate(expr)
 
+
+if __name__ == "__main__":
+    sys.exit(main())
